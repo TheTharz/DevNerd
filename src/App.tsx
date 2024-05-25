b722fccb9f5b22e5537050f5d10cf8d72f286e96
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-05-25 23:30:11 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,12 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route path='/DevNerd' element={<HomePage />} />
+        <Route path='/DevNerd/about' element={<AboutPage />} />
+
+        {/* Error route goes here */}
+
+        <Route path='*' element={<ErrorPage />} />
 
         {/*Blog routes are added here */}
         {blogs.map((blog, index) => (
@@ -17,12 +22,6 @@ function App() {
             element={<blog.Component />}
           />
         ))}
-
-        <Route path='/about' element={<AboutPage />} />
-
-        {/* Error route goes here */}
-
-        <Route path='*' element={<ErrorPage />} />
       </Routes>
     </Router>
   );
